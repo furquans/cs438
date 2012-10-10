119547c6a1993fc6ec360f4ca634c8e47f6933dc
Furquan Shaikh <furquan.m.shaikh@gmail.com>
2012-10-10 02:38:40 -0500

    Step 2 minor changes
    Signed-off-by: Furquan Shaikh <furquan.m.shaikh@gmail.com>

--- MP2/router.c
+++ MP2/router.c
@@ -6,6 +6,12 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
+#define HELO_MSG  "HELO\n"
+#define READY_MSG "READY\n"
+#define NEIGH_MSG "NEIGH?\n"
+
+typedef unsigned char bool;
+
 int create_tcp_connection(char *hostname,
 			  char *port)
 {
@@ -52,9 +58,9 @@ int create_tcp_connection(char *hostname,
 }
 
 void send_msg_to_manager(int sockfd,
-			 char *msg,
-			 int count)
+			 char *msg)
 {
+	int count = strlen(msg);
 	if (send(sockfd,
 		 msg,
 		 count,
@@ -64,8 +70,8 @@ void send_msg_to_manager(int sockfd,
 	}
 }
 
-int recv_msg_from_manager(int sockfd,
-			  char *msg)
+bool recv_msg_from_manager(int sockfd,
+			   char *msg)
 {
 	int ret;
 #define MAX_MSG_LEN 100
@@ -85,12 +91,10 @@ void get_addr_from_manager(int sockfd,
 			   char *myaddr)
 {
 #define ADDR_STR_LEN 25
-	char msg[] = "HELO\n";
 	char addr[MAX_MSG_LEN];
 
 	send_msg_to_manager(sockfd,
-			    msg,
-			    strlen(msg));
+			    HELO_MSG);
 
 	recv_msg_from_manager(sockfd,
 			      addr);
@@ -101,7 +105,7 @@ void get_addr_from_manager(int sockfd,
 	printf("Received string:%s\n",addr);
 }
 
-int send_udp_details_to_manager(int sockfd,
+bool send_udp_details_to_manager(int sockfd,
 				char *hostname,
 				char *udpport)
 {
@@ -110,7 +114,7 @@ int send_udp_details_to_manager(int sockfd,
 
 	sprintf(msg, "HOST %s %s\n",hostname, udpport);
 
-	send_msg_to_manager(sockfd, msg, strlen(msg));
+	send_msg_to_manager(sockfd, msg);
 
 	recv_msg_from_manager(sockfd, resp);
 
@@ -118,18 +122,28 @@ int send_udp_details_to_manager(int sockfd,
 		       "OK") == 0);
 }
 
-void get_neigh_details_from_manager(int sockfd)
+int get_neigh_details_from_manager(int sockfd)
 {
-	char msg[] = "NEIGH?\n";
 	char resp[MAX_MSG_LEN];
+	char *ptr;
 
-	send_msg_to_manager(sockfd, msg, strlen(msg));
+	send_msg_to_manager(sockfd, NEIGH_MSG);
 
 	do {
 		memset(resp, 0, MAX_MSG_LEN);
 		recv_msg_from_manager(sockfd, resp);
+		ptr = strchr(resp, '\n');
+		if (ptr) {
+			*ptr = '\0';
+			ptr++;
+		}
 		printf("received msg:%s\n",resp);
-	} while(strcmp(resp, "DONE"));
+	} while((ptr == NULL) || strcmp(ptr, "DONE"));
+
+	send_msg_to_manager(sockfd, READY_MSG);
+	recv_msg_from_manager(sockfd, resp);
+	return (strcmp(resp,
+		       "OK") == 0);
 }
 
 int main(int argc, char **argv)
@@ -157,11 +171,15 @@ int main(int argc, char **argv)
 	if (!send_udp_details_to_manager(sockfd,
 					 argv[1],
 					 argv[3])) {
-		printf("Error from manager\n");
+		printf("Error from manager in step 1\n");
 		exit(1);
 	}
 
-	get_neigh_details_from_manager(sockfd);
+	if (!get_neigh_details_from_manager(sockfd)) {
+		printf("Error from manager in step 2\n");
+		exit(1);
+	}
 
+	printf("success\n");
 	close(sockfd);
 }
